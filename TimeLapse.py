ccbd9945246083c3cf0b3aaccd96ba9bc89e749b
“Owen <orcasciotti@comcast.net>
2023-02-12 13:55:47 -0500

    Modified timelapse to run at set interval and write to temp file

--- TimeLapse.py
+++ TimeLapse.py
@@ -3,6 +3,7 @@
 from glob import glob
 from cv2 import VideoWriter, imread, VideoWriter_fourcc, resize
 import time
+import os
 
 #to run shell scripts
 import subprocess
@@ -42,35 +43,40 @@ def write_video (vid_writer: VideoWriter, imgs: list) -> None:
     print (f"Wrote {len(imgs)} files")
 
 
-folder_path = "/Volumes/CaileyHP/Capstone timelapse sample/Test sample 2/*.JPG"
+folder_path = "/media/pi/T7/Test3/*.jpg"
+temp_video_name = "static/temp.mp4"
+video_name = "static/Timelapse.mp4"
 files_read = []
 loaded_imgs = []
 fourcc = VideoWriter_fourcc('m', 'p', '4', 'v')
 width, height = 720, 480
-
+start_time = time.time()
+timeout = 30 * 60
 
 while True:
     try:
-        current_files = get_filenames(folder_path)
-        if files_read == []:
-            video_obj = VideoWriter("static/Timelapse.mp4", fourcc, 30, (width, height))
-            loaded_imgs = load_imgs(current_files, loaded_imgs, width, height)
-            write_video (video_obj, loaded_imgs)
-            files_read = current_files
-            print("First time writing")
+	if time.time() - start_time >= timeout:
+            start_time = time.time()
+            current_files = get_filenames(folder_path)
+            if files_read == []:
+                video_obj = VideoWriter(temp_video_name, fourcc, 30, (width, height))
+                loaded_imgs = load_imgs(current_files, loaded_imgs, width, height)
+                write_video (video_obj, loaded_imgs)
+                files_read = current_files
+                print("First time writing")
+                os.rename(temp_video_name, video_name)
 
-        if len(current_files) > len(files_read):
-            video_obj = VideoWriter("static/Timelapse.mp4", fourcc, 30, (width, height))
-            new_files = current_files[len(files_read):]
-            loaded_imgs = load_imgs (new_files, loaded_imgs, width, height)
-            write_video(video_obj, loaded_imgs)
-            files_read = files_read + new_files
-            print ("Wrote new files")
-        else:
-            print ("No new files")
+            if len(current_files) > len(files_read):
+                video_obj = VideoWriter(temp_video_name, fourcc, 30, (width, height))
+                new_files = current_files[len(files_read):]
+                loaded_imgs = load_imgs (new_files, loaded_imgs, width, height)
+                write_video(video_obj, loaded_imgs)
+                files_read = files_read + new_files
+                print ("Wrote new files")
+                os.rename(temp_video_name, video_name)
+            else:
+                print ("No new files")
 
-        if len(current_files) == len(files_read):
-            time.sleep(30)
 
     except KeyboardInterrupt:
         break
