ecd4e82523087625c11fbf17381e6855946d94d6
sjr9726 <sjr9726@rit.edu>
2023-03-03 17:01:43 -0500

    capture images

--- capture_images.py
+++ capture_images.py
@@ -2,6 +2,7 @@
 Has a thread for capturing images
 """
 import time
+from datetime import date
 from glob import glob
 
 import gphoto2 as gp
@@ -9,33 +10,31 @@ import threading
 
 
 class CaptureImageThread(threading.Thread):
-    def __init__(self, image_template: str, duration_s: int, frequency_s: int, image_files: list):
+    def __init__(self, image_folder: str, num_images: int, frequency_s: int, image_files: list):
         super().__init__()
         self.image_files = image_files
-        self.image_template = image_template
+        self.image_folder = image_folder
         self.frequency_s = frequency_s
-        self.duration_s = duration_s
+        self.num_images = num_images
 
         self.daemon = True  # This sets the thread to end when the main program ends
 
     def run(self):
         camera = gp.Camera()
-        start_time = time.time()
         most_recent_capture_time = 0
         count = 0
-        while time.time() < start_time + self.duration_s:
-            try:
-                if most_recent_capture_time < time.time() + self.frequency_s:
-                    path = camera.capture(gp.GP_CAPTURE_IMAGE)
-                    most_recent_capture_time = time.time()
-                    camera_file = camera.file_get(path.folder, path.name, gp.GP_FILE_TYPE_NORMAL)
-                    camera_file.save(self.image_template % count)
-                    camera.file_delete(path.folder, path.name)
-                    self.image_files.append(self.image_template % count)
-                    count += 1
-            except:
-                print("The Camera is disconnected")  # This will just spam print if the camera is disconnected
-                camera = gp.Camera()
+        while len(self.image_files) < self.num_images:
+
+            if most_recent_capture_time < time.time() + self.frequency_s:
+                path = camera.capture(gp.GP_CAPTURE_IMAGE)
+                most_recent_capture_time = time.time()
+                camera_file = camera.file_get(path.folder, path.name, gp.GP_FILE_TYPE_NORMAL)
+                image_file=self.image_folder+f"/Image_{time.strftime('%Y_%m_%d_%S')}.jpg"
+                camera_file.save(image_file)
+                camera.file_delete(path.folder, path.name)
+                self.image_files.append(image_file)
+                count += 1
+
 
 
 class AbstractCamera(threading.Thread):
@@ -53,21 +52,64 @@ class AbstractCamera(threading.Thread):
         while True:
             files = glob(self.image_folder)
             files = sorted(files)
-            print(f"Files retrieved: {len(files)}")
             self.image_files.clear()
             self.image_files.extend(files)
             time.sleep(10)
 
 
-def main():
+def get_new_images(image_files, num_images_obtained):
+    return image_files[num_images_obtained:]
+
+
+def check_finshed(capture_image_thread):
+    return not capture_image_thread.is_alive()
+
+
+def start_capture(frequency_s: int, num_images: int, image_folder: str):
+    """
+    Starts a capture of the image
+    @param frequency_s: Wait time between images in seconds
+    @param num_images: Number of images to be taken
+    @param image_folder: The folder where the images get saved to
+    @returns get_images: function that gets new images with the parameter of the number of images already obtained
+    @returns is_finished: function that checks whether the code is still capturing
+    """
     image_files = []
-    capture_image = CaptureImageThread('SampleImages/frame%04d.jpg', 20, 2, image_files)
-    capture_image.start()
-    time.sleep(4)
-    print(image_files)
-    capture_image.join()
-    print(image_files)
+    image_capture = CaptureImageThread(image_folder, num_images, frequency_s, image_files)
+    image_capture.start()
+
+    return lambda num_image_obtained: get_new_images(image_files, num_image_obtained), lambda: check_finshed(
+        image_capture)
+
+
+def start_abstract_capture(image_folder):
+    """
+    treats a folder as if it were a camera and updates the new images every 10 seconds
+    @param image_folder: the folder with images
+    @returns get_images: function that
+    """
+    image_files = []
+    image_capture = AbstractCamera(image_folder, image_files)
+    image_capture.start()
+
+    return lambda num_image_obtained: get_new_images(image_files, num_image_obtained)
 
 
 if __name__ == "__main__":
-    main()
+    """
+    This is an example of calling this 
+    Real image capture: 
+    """
+    print(gp.gp_library_version(False))
+    # Real image capturing: capturing images every 3 seconds for a total of 5 images
+    get_images, is_finished = start_capture(3, 5, "SampleImageLocation")
+    while not is_finished():
+        print(get_images(0))
+        time.sleep(5)
+    print(get_images(0))
+    print(get_images(3))
+
+    # Treating folder as capturing
+    get_images = start_abstract_capture("SampleImageLocation/*.jpg")
+    time.sleep(1)
+    print(get_images(0))
