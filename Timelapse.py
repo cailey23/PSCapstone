d4d91e7648b65f442a93d6d5cda1ad80667ecaee
1593726048 <StephenJRinaldo@gmail.com>
2023-04-06 11:47:30 -0400

    Integration of capture images

--- Timelapse.py
+++ Timelapse.py
@@ -1,13 +1,18 @@
+from typing import Tuple
+
 import cv2
 import os
 import re
-def TimeLapse ():
+
+from capture_images import start_capture
+
+
+def TimeLapse (fps: int = 30, resolution: Tuple[int, int] = (3840, 2160), frequency_s: int = 10,
+               num_images_to_capture: int = 30):
     # set the path to the folder containing the images
     path = '/Volumes/CaileyHP/CapstoneTimelapseSample/TestSample1'
 
     # set the frame rate and resolution
-    fps = 30
-    resolution = (3840, 2160)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
     # a variable that numbers the output videos
@@ -19,7 +24,9 @@ def TimeLapse ():
     # a variable that keeps track of which frames have been processed already
     num_processed = 0
 
-    while True:
+    get_images, is_finished = start_capture(frequency_s=frequency_s, num_images=num_images_to_capture, image_folder=path)
+
+    while not is_finished():
         # get the list of all the image files in the folder
         images = [img for img in os.listdir(path) if img.endswith('.JPG')]
 
