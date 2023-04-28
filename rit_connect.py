548eb030a67f9f70b082c96c1cfba3983ce9b511
1593726048 <StephenJRinaldo@gmail.com>
2023-04-28 15:43:48 -0400

    Resize if too large

--- rit_connect.py
+++ rit_connect.py
@@ -1,15 +1,20 @@
+import os
 
-
+from moviepy.editor import VideoFileClip
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-
 def upload_video(video_name):
+    while os.path.getsize(video_name)> 85*1000000:
+        video = VideoFileClip(video_name)
+        video_resized = video.resize(0.7)
+        video_name="video_resized.mp4"
+        video_resized.write_videofile(video_name)
     options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
+    options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
-    # options.add_argument("disable-gpu")
+    options.add_argument("disable-gpu")
 
     browser = webdriver.Chrome('chromedriver', options=options)
     browser.get('https://www.rit.edu/marketing/utilities/feeds/time-lapse/')
